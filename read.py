6de09a18d2e4441644bcd4530d6b53dcd8e374de
Jason Ou <lloivne4119@gmail.com>
2021-02-14 00:32:57 +0800

    filter good

--- read.py
+++ read.py
@@ -26,3 +26,11 @@ for d in data:
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言中有 good')
+print(good[0])
+print(good[1])
